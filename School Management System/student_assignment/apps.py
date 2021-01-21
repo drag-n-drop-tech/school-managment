93bd3a706e3fe64d1cab0308b5e493204b383cef
zaakir hussain <zaakir@gmail.com>
2021-01-21 16:38:40 +0530

    Created assignment and fees app

---
+++ School Management System/student_assignment/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StudentAssignmentConfig(AppConfig):
+    name = 'student_assignment'
---
+++ School Management System/student_fee/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StudentFeeConfig(AppConfig):
+    name = 'student_fee'
